b893c3771eb588d590755bdb5592dfd5847cc9b1
Josh Jones <ohblonddev@gmail.com>
2019-12-26 14:19:25 +0000

    add get_fuel_requirement

--- src/twenty_nineteen/day_one.rs
+++ src/twenty_nineteen/day_one.rs
@@ -1,3 +1,21 @@
 pub fn thingy() {
     println!("hello there");
 }
+
+pub fn get_fuel_requirement(mass: i64) -> i64 {
+    let floor_divide_by_3: i64 = mass / 3;
+    floor_divide_by_3 - 2
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::twenty_nineteen::day_one::get_fuel_requirement;
+
+    #[test]
+    fn get_fuel_requirement_test() {
+        assert_eq!(get_fuel_requirement(12), 2);
+        assert_eq!(get_fuel_requirement(14), 2);
+        assert_eq!(get_fuel_requirement(1969), 654);
+        assert_eq!(get_fuel_requirement(100756), 33583);
+    }
+}
